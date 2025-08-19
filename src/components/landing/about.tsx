bd1755a3f485cf8110363640e516c73cad6edbe6
Ram tom <ramtom1008@gmail.com>
2025-08-19 07:23:25 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <p>
    
    with the text

--- src/components/landing/about.tsx
+++ src/components/landing/about.tsx
@@ -14,6 +14,7 @@ export default function About() {
       }}
       aria-labelledby="about-heading"
     >
+      <div className="absolute inset-0 bg-black/50"></div>
       
       <div className="container relative z-10 mx-auto max-w-[1200px] px-6 md:px-12">
         <div className="max-w-2xl text-left">
@@ -23,7 +24,7 @@ export default function About() {
             <div>We Are</div>
           </h2>
           <div className="space-y-4 text-base leading-relaxed text-white md:text-lg">
-            <p>
+            <p className="text-white">
               We are a Full Service Sign Company with Over 20 Years in the Industry. We specialize in Commercial Building Signs, Storefront Signs and Monument Signs!
             </p>
             <ul className="list-disc list-inside space-y-1 text-left">
@@ -37,7 +38,7 @@ export default function About() {
               <li className="text-white">Permit Processing</li>
               <li className="text-white">Licensed, Bonded and Insured</li>
             </ul>
-            <p>
+            <p className="text-white">
               Give us a call today for a Free Estimate!
             </p>
           </div>
