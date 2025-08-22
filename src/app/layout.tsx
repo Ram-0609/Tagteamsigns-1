ef43b21fe3d6ab79e0e47eb18f9f1419843303de
Ram tom <ramtom1008@gmail.com>
2025-08-22 08:49:32 +0000

    add instagram chatbox in right bottom corner

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import type {Metadata} from 'next';
 import './globals.css';
 import { Toaster } from "@/components/ui/toaster"
 import React, { useEffect } from 'react';
+import InstagramChat from '@/components/landing/InstagramChat';
 
 export default function RootLayout({
   children,
@@ -72,6 +73,7 @@ export default function RootLayout({
       </head>
       <body className="font-body antialiased">
         {children}
+        <InstagramChat />
         <Toaster />
       </body>
     </html>
